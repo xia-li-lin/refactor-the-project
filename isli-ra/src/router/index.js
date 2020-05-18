f99c2087822961cdcdf3802b2a6a614821cd733e
xia-li-lin <1093492176@qq.com>
2020-05-18 11:53:01 +0800

    '在项目中使用scss'

--- isli-ra/src/router/index.js
+++ isli-ra/src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
+
+const LoginComponent = () => import('../pages/login/Login.vue')
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'login',
+      component: LoginComponent
     }
   ]
 })
